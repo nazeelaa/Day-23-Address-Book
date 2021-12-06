6b5dc120b8cde3e6ddda999922d7b618b31e32b5
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-12-06 17:07:13 +0530

    Uc1 creating Address Book

---
+++ Day23_AddressBook/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day24_AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Address Book Program \n");
+
+            AddressBook.AddContact();
+            AddressBook.Display();
+        }
+    }
+}
